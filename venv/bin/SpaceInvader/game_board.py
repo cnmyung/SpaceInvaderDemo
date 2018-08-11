eec575ce6dac9f4ac982fb743c46e9fb6d677cf8
Chris Myung <christopher_myung@yahoo.com>
2018-08-10 21:31:15 -0400

    spaceship added with lateral movement capability

--- venv/bin/SpaceInvader/game_board.py
+++ venv/bin/SpaceInvader/game_board.py
@@ -1,19 +1,30 @@
-import sys
 import pygame
+
+from settings import Settings
+from ship import Ship
+import event_handlers as e_handlers
 
 
 def run_game():
     """Initialize game and create a screen object"""
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    board_settings = Settings()
+    screen = pygame.display.set_mode((board_settings.screen_width,
+                                      board_settings.screen_height))
+
     pygame.display.set_caption("Space Invaders")
+    ship = Ship(screen, board_settings)
 
     # starts main for the game
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        e_handlers.check_events(ship)
+        ship.updatePos()
+        e_handlers.update_screen(board_settings,screen,ship)
+
+        screen.fill(board_settings.bg_color)
+        ship.place_ship()
 
         pygame.display.flip()
+
 
 run_game()
---
+++ venv/bin/SpaceInvader/ship.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, screen, board_settings):
+        """initialize the ship to starting position on screen"""
+        self.screen = screen
+        self.board_settings = board_settings
+
+        self.image = pygame.image.load('images/ship.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # aligns ship to start in the bottom-center of screen
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        # movements
+        self.moving_right = False
+        self.moving_left = False
+
+    def updatePos(self):
+        """updates the ship's position"""
+        if self.moving_right:
+            self.center += self.board_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.board_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
+
+
+    def place_ship(self):
+        self.screen.blit(self.image,self.rect)
